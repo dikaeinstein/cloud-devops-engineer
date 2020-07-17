26ab4608926b2bcc8e688fa62b319edec4cbd379
Solomon Okwa <solozyokwa@gmail.com>
2020-07-17 23:29:16 +0100

    switch back to local state

--- main.tf
+++ main.tf
@@ -1,10 +1,10 @@
-module "s3_state_backend" {
-  source = "./s3-backend"
+# module "s3_state_backend" {
+#   source = "./s3-backend"
 
-  bucket_name       = "cde-nanodegree-infra-tfstate"
-  dynamo_table_name = "cde-nanodegree-infra-tfstate-lock"
-  tag_name          = "UdacityCloudDevopsEngineerNanodegree"
-}
+#   bucket_name       = "cde-nanodegree-infra-tfstate"
+#   dynamo_table_name = "cde-nanodegree-infra-tfstate-lock"
+#   tag_name          = "UdacityCloudDevopsEngineerNanodegree"
+# }
 
 # module "static_site" {
 #   source = "./static-site"
--- providers.tf
+++ providers.tf
@@ -1,12 +1,12 @@
-terraform {
-  backend "s3" {
-    bucket = "cde-nanodegree-infra-tfstate"
-    key    = "terraform.state"
-    region = "eu-west-2"
+# terraform {
+#   backend "s3" {
+#     bucket = "cde-nanodegree-infra-tfstate"
+#     key    = "terraform.state"
+#     region = "eu-west-2"
 
-    dynamodb_table = "cde-nanodegree-infra-tfstate-lock"
-  }
-}
+#     dynamodb_table = "cde-nanodegree-infra-tfstate-lock"
+#   }
+# }
 
 provider "aws" {
   version = "~> 2.0"
